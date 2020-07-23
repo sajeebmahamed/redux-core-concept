ee1c5bfe24d15d2dd94f912d8d47e2c5acd66854
Sajeeb <sajeebahamed6@gmail.com>
2020-07-23 14:33:32 +0600

    redux core

--- src/App.js
+++ src/App.js
@@ -1,47 +1,54 @@
 import React, {Component} from 'react'
 import './App.css'
-import { createStore } from 'redux'
+
+import {Provider} from 'react-redux'
+import store from './store'
+import Count from './components/Count'
+import Control from './components/Control'
 
 class App extends Component {
   render() {
 
-    const reducer = (state={}, action) => {
+    // const reducer = (state={}, action) => {\
+    //   if(action.type === 'A') {
+    //     return {
+    //       ...state,
+    //       A: 'Im A'
+    //     }
+    //   }
+    //   if(action.type === 'B') {
+    //     return {
+    //       ...state,
+    //       B: 'Im B'
+    //     }
+    //   }
 
-      if(action.type === 'A') {
-        return {
-          ...state,
-          A: 'Im A'
-        }
-      }
-      if(action.type === 'B') {
-        return {
-          ...state,
-          B: 'Im B'
-        }
-      }
+    //   return state
+    // }
+    // const store = createStore(reducer)
 
-      return state
-    }
-    const store = createStore(reducer)
+    // store.subscribe(() => {
+    //   console.log(store.getState().A)
+    // })
+    // store.subscribe(() => {
+    //   console.log(store.getState().B)
+    // })
 
-    store.subscribe(() => {
-      console.log(store.getState().A)
-    })
-    store.subscribe(() => {
-      console.log(store.getState().B)
-    })
-
-    store.dispatch({type: 'A'})
-    store.dispatch({type: 'Something'})
-    store.dispatch({type: 'B'})
-    store.dispatch({type: 'Something'})
+    // store.dispatch({type: 'A'})
+    // store.dispatch({type: 'Something'})
+    // store.dispatch({type: 'B'})
+    // store.dispatch({type: 'Something'})
 
 
 
     return(
-      <div className="App">
-        <h1>Redux World</h1>
-      </div>
+      <Provider store= {store}>
+        <div className="App">
+          <h1>Redux World</h1>
+          <Count/>
+          <Control/>
+        </div>
+      </Provider>
     );
   }
 }
